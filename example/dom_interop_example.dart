c9c658eeb633bf647f73cf6aa4ce7b2c257dfe35
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-03-07 18:36:39 +0900

    Add an example.

--- example/dom_interop_example.dart
+++ example/dom_interop_example.dart
@@ -1,11 +1,13 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 library dom_interop.example;
+
+import 'dart:html';
 
 import 'package:dom_interop/dom_interop.dart';
 
 main() {
-  var awesome = new Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  document.querySelector('#text').onClick.listen((event) {
+    final position = caretPositionFromPoint(event.client);
+    document.querySelector('#content').innerHtml =
+        '{offsetNode: ${position.offsetNode}, offset: ${position.offset}}';
+  });
 }
